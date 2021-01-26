4ee691c2be3ab4990a77c86eea939107f9a80e94
damirbaisalov <baisalovdamir@gmail.com>
2021-01-26 22:51:45 +0600

    Добавил верстку для авторизации

--- app/src/main/java/com/example/mobiletest/MainActivity.kt
+++ app/src/main/java/com/example/mobiletest/MainActivity.kt
@@ -1,11 +1,16 @@
 package com.example.mobiletest
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.mobiletest.login.LoginActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val intent = Intent(this@MainActivity, LoginActivity::class.java)
+        startActivity(intent)
     }
 }
---
+++ app/src/main/java/com/example/mobiletest/login/LoginActivity.kt
@@ -0,0 +1,12 @@
+package com.example.mobiletest.login
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.mobiletest.R
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
