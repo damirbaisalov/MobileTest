697fa996993a1727d1b205b1ee2fc1803acb0a4e
damirbaisalov <baisalovdamir@gmail.com>
2021-01-27 18:35:26 +0600

    Добавил верстку для регистрации

--- app/src/main/java/com/example/mobiletest/registration/RegistrationActivity.kt
+++ app/src/main/java/com/example/mobiletest/registration/RegistrationActivity.kt
@@ -2,11 +2,24 @@ package com.example.mobiletest.registration
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageView
 import com.example.mobiletest.R
 
 class RegistrationActivity : AppCompatActivity() {
+
+    private lateinit var accountImageView: ImageView
+    private lateinit var accountAddImageButton: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_registration)
+
+        initViews()
+    }
+
+    private fun initViews() {
+        accountImageView = findViewById(R.id.account_image)
+        accountAddImageButton = findViewById(R.id.account_add_photo_button)
     }
 }
