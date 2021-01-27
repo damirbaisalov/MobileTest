2ab4fce388589314260716300f67eeaf494849a8
damirbaisalov <baisalovdamir@gmail.com>
2021-01-28 00:21:05 +0600

    Добавил логику и исправил верстку

--- app/src/main/java/com/example/mobiletest/registration/RegistrationActivity.kt
+++ app/src/main/java/com/example/mobiletest/registration/RegistrationActivity.kt
@@ -5,18 +5,27 @@ import android.content.Intent
 import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Button
-import android.widget.EditText
-import android.widget.ImageView
+import android.text.TextUtils
+import android.view.Gravity
+import android.widget.*
 import com.example.mobiletest.R
 import java.util.*
+import kotlin.math.sign
 
 const val IMAGE_CODE = 1
 class RegistrationActivity : AppCompatActivity() {
 
     private lateinit var accountImageView: ImageView
     private lateinit var accountAddImageButton: Button
+    private lateinit var accountNameEditText: EditText
+    private lateinit var accountSurnameEditText: EditText
+    private lateinit var accountThirdNameEditText: EditText
     private lateinit var accountDateEditText: EditText
+    private lateinit var accountCityEditText: EditText
+    private lateinit var accountAboutUserEditText: EditText
+    private lateinit var licenseCheckBox: CheckBox
+    private lateinit var licenseLinearLayout: LinearLayout
+    private lateinit var signUpButton: Button
     private var uriData: Uri?=null
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -34,11 +43,27 @@ class RegistrationActivity : AppCompatActivity() {
             val month = calendarData.get(Calendar.MONTH)
             val year = calendarData.get(Calendar.YEAR)
 
-            val dpd = DatePickerDialog(this, DatePickerDialog.OnDateSetListener { view, year, monthOfYear, dayOfMonth ->
+            val dpd = DatePickerDialog(
+                    this,
+                    DatePickerDialog.OnDateSetListener { view, year, monthOfYear, dayOfMonth ->
                 accountDateEditText.setText(""+ dayOfMonth + "/" + (monthOfYear + 1) + "/" + year)
-            }, year, month, day)
+            },
+                    year,
+                    month,
+                    day
+            )
 
             dpd.show()
+        }
+        signUpButton.setOnClickListener {
+            if (licenseCheckBox.isChecked && !TextUtils.isEmpty(accountNameEditText.toString()) && !TextUtils.isEmpty(accountSurnameEditText.toString()))
+            { toastMessage("Успешная регистрация") }
+            else
+                if (!licenseCheckBox.isChecked) {
+                    toastMessage("Подтвердите пользовательское соглашение")
+                } else {
+                    toastMessage("Заполните необходимые поля")
+                }
         }
     }
 
@@ -46,6 +71,14 @@ class RegistrationActivity : AppCompatActivity() {
         accountImageView = findViewById(R.id.account_image)
         accountAddImageButton = findViewById(R.id.account_add_photo_button)
         accountDateEditText = findViewById(R.id.account_date)
+        accountNameEditText = findViewById(R.id.account_name)
+        accountSurnameEditText= findViewById(R.id.account_surname)
+        accountThirdNameEditText = findViewById(R.id.account_third_name)
+        accountCityEditText = findViewById(R.id.account_city)
+        accountAboutUserEditText = findViewById(R.id.account_about)
+        licenseCheckBox = findViewById(R.id.checkbox)
+        licenseLinearLayout = findViewById(R.id.activity_registration_license_linear_layout)
+        signUpButton = findViewById(R.id.activity_registration_sign_up_button)
     }
 
     private fun openImageForm() {
@@ -63,6 +96,16 @@ class RegistrationActivity : AppCompatActivity() {
             uriData = data.data
             accountImageView.setImageURI(uriData)
         }
+    }
+
+    private fun toastMessage(message: String) {
+        val toast = Toast.makeText(
+                this,
+                message,
+                Toast.LENGTH_LONG
+        )
+        toast.setGravity(Gravity.CENTER, 0, 0)
+        toast.show()
     }
 
 }
