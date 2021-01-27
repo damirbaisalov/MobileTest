5ce7bcd5d4dfb2351018a3450e68e18718f88014
damirbaisalov <baisalovdamir@gmail.com>
2021-01-27 19:44:19 +0600

    Добавил функцию добавления фотографии на аватарку

--- app/src/main/java/com/example/mobiletest/registration/RegistrationActivity.kt
+++ app/src/main/java/com/example/mobiletest/registration/RegistrationActivity.kt
@@ -1,25 +1,51 @@
 package com.example.mobiletest.registration
 
+import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.ImageView
 import com.example.mobiletest.R
 
+const val IMAGE_CODE = 1
 class RegistrationActivity : AppCompatActivity() {
 
     private lateinit var accountImageView: ImageView
     private lateinit var accountAddImageButton: Button
+    private var uriData: Uri?=null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_registration)
 
         initViews()
+        accountAddImageButton.setOnClickListener {
+            openImageForm()
+        }
+
     }
 
     private fun initViews() {
         accountImageView = findViewById(R.id.account_image)
         accountAddImageButton = findViewById(R.id.account_add_photo_button)
     }
+
+    private fun openImageForm() {
+        val intent = Intent()
+        intent.type = "image/*"
+        intent.action = Intent.ACTION_GET_CONTENT
+        startActivityForResult(intent, IMAGE_CODE)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if (requestCode== IMAGE_CODE && resultCode== RESULT_OK && data!=null && data.data!=null) {
+
+            uriData = data.data
+            accountImageView.setImageURI(uriData)
+        }
+    }
+
 }
