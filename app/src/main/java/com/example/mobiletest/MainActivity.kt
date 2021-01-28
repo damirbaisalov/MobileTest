51023e57fc6d91e3aa374a36c0b7d732fdff8516
damirbaisalov <baisalovdamir@gmail.com>
2021-01-28 15:11:11 +0600

    Добавил статус бар для splash screena

--- app/src/main/java/com/example/mobiletest/MainActivity.kt
+++ app/src/main/java/com/example/mobiletest/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.mobiletest
 
+import android.animation.ObjectAnimator
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
@@ -8,40 +9,28 @@ import android.widget.ProgressBar
 import com.example.mobiletest.login.LoginActivity
 import java.util.*
 
+private const val TIME_DURATION_PROGRESS = 3000
 class MainActivity : AppCompatActivity() {
 
     private val splashTime: Long = 3000
     private lateinit var progressBar: ProgressBar
-    private lateinit var timer: Timer
-    private val counter = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         progressBar = findViewById(R.id.progress_bar)
+
+        progressBar.max = TIME_DURATION_PROGRESS
+        val currentProgress = TIME_DURATION_PROGRESS
+        ObjectAnimator.ofInt(progressBar, "progress", currentProgress)
+                .setDuration(splashTime)
+                .start()
+
         Handler().postDelayed({
             val intent = Intent(this@MainActivity, LoginActivity::class.java)
             startActivity(intent)
             finish()
         }, splashTime)
     }
-//        Thread{
-//                doLoadingWork()
-//                val intent = Intent(this@MainActivity, LoginActivity::class.java)
-//                startActivity(intent)
-//                finish()
-//        }.run()
-//    }
-//
-//    private fun doLoadingWork() {
-//        for(progress in 0..50 step 10){
-//            try{
-//                Thread.sleep(1000)
-//                progressBar.progress = progress
-//            } catch (e: Exception) {
-//                Toast.makeText(this,e.toString(),Toast.LENGTH_LONG).show()
-//            }
-//        }
-//    }
 }
